6c4d7ebb305ed96f884c1590695a9e196c22fe22
Tolunay <tlnymtlu@gmail.com>
2024-02-26 06:12:45 +0300

    changed logic and flags

--- cmd/update.go
+++ cmd/update.go
@@ -13,6 +13,10 @@ import (
 )
 
 // updateCmd represents the update command
+
+var home, _ = os.UserHomeDir()
+var d3uHomeDB = home + "\\d3u\\db"
+var d3uHomeDLSS = home + "\\d3u\\dlss"
 var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "A brief description of your command",
@@ -20,53 +24,58 @@ var updateCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 
 		version, _ := cmd.Flags().GetString("version")
-		gameSelection, _ := cmd.Flags().GetString("game")
+		customDLL := d3uHomeDLSS + "\\nvngx_dlss_" + version + ".dll"
 
-		downloadLink := "https://github.com/TolunayM/dlss-repo/releases/download/" + version + "/nvngx_dlss.dll"
-		db, err := bolt.Open("my.bboltconnection", 0600, nil)
+		/*
+			this flag was used for get the games that wanted to be updated
+			but after thinking about that I decide to change
+			on cobra-cli all commands actually flags of root command
+			and that means you can already have parse flag as named "args"
+			this way is better for "command line philosophy"
+		*/
+		//gameSelection := cmd.Flags().Parse("game")
+
+		db, err := bolt.Open(d3uHomeDB+"\\my.bboltconnection", 0600, nil)
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer db.Close()
 
-		fmt.Println("Downloading dlss files.\nThis may take a minute based on your connection speed.")
-
-		download := exec.Command(
-			"curl",
-			"-OL",
-			downloadLink)
-		_, err = download.Output()
-
+		// check before download if requested dlss file is downloaded before
+		file, err := os.ReadFile(customDLL)
 		if err != nil {
-			_ = fmt.Errorf("Something happened %s", err)
+			fmt.Println("Dlss files is not present locally download starting...")
+			downloadDLSS(version)
+			file, _ = os.ReadFile(customDLL)
 		}
 
-		file, err := os.ReadFile("nvngx_dlss.dll")
-		if err != nil {
-			fmt.Println(err)
-		}
-
+		// updating
 		db.View(func(tx *bolt.Tx) error {
 			b := tx.Bucket([]byte("MyBucket"))
 			c := b.Cursor()
 
-			for key, value := c.First(); key != nil; key, value = c.Next() {
-				if gameSelection != "" {
+			if len(args) != 0 {
+				for cursor := 0; cursor < len(args); cursor++ {
+					if string(b.Get([]byte(args[cursor]))) != "" {
+						err = os.WriteFile(string(b.Get([]byte(args[cursor])))+"\\nvngx_dlss.dll", file, 0644)
+						fmt.Println(args[cursor] + " Updated to " + version + " Successfully")
+					} else {
+						fmt.Println("Game is not present check your typo or use getgames command for listing games added to database")
+					}
 
-					fmt.Println(gameSelection + " Updated to " + version + " Successfully")
-					err = os.WriteFile(string(b.Get([]byte(gameSelection)))+"\\nvngx_dlss.dll", file, 0644)
+				}
 
-					return nil
+			} else {
 
-				} else {
+				for key, value := c.First(); key != nil; key, value = c.Next() {
 
 					fmt.Println(string(key) + " Updated to " + version + " Successfully")
 					err = os.WriteFile(string(value)+"\\nvngx_dlss.dll", file, 0644)
 
-				}
+					if err != nil {
+						fmt.Println(err)
+					}
 
-				if err != nil {
-					fmt.Println(err)
 				}
 
 			}
@@ -79,6 +88,27 @@ var updateCmd = &cobra.Command{
 func init() {
 
 	updateCmd.Flags().StringP("version", "v", "latest", "Version specifier")
-	updateCmd.Flags().StringP("game", "g", "", "Game selection for updating specific games")
+	//updateCmd.Flags().StringP("game", "g", "", "Game selection for updating specific games")
+
 	rootCmd.AddCommand(updateCmd)
+}
+
+func downloadDLSS(version string) {
+
+	downloadLink := "https://github.com/TolunayM/dlss-repo/releases/download/" + version + "/nvngx_dlss.dll"
+	customDLL := d3uHomeDLSS + "\\nvngx_dlss_" + version + ".dll"
+
+	fmt.Println("Downloading dlss files.\nThis may take a minute based on your connection speed.")
+	download := exec.Command(
+		"curl",
+		"-o",
+		customDLL,
+		"-L",
+		downloadLink)
+	_, err := download.Output()
+
+	if err != nil {
+		_ = fmt.Errorf("something happened %s", err)
+	}
+
 }
