0e95f025fca16e8a064f5a499d1d7b1f17f0d2bc
Tolunay <tlnymtlu@gmail.com>
2024-02-26 06:09:03 +0300

    boiler comments remove

--- cmd/remove.go
+++ cmd/remove.go
@@ -27,16 +27,6 @@ var rmCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(rmCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// removeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// removeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func removeLocation(removedFile string) {
